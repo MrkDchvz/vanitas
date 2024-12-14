93918a0a9e4259b908ae194c04ed3d7fb26a849b
Mrkdchvz <madechavez22@gmail.com>
2024-12-14 17:57:39 +0800

    fix: fixed routes for static files for pages

--- vanitas/vanitas/settings.py
+++ vanitas/vanitas/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'entry.apps.EntryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'entry',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
