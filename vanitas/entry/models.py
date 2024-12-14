7d105d398df2e27dc5d1e6187d94032cffda9fc1
Mrkdchvz <madechavez22@gmail.com>
2024-12-14 13:34:48 +0800

    feat: added models for system

---
+++ vanitas/entry/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Mood(models.Model):
+    name = models.CharField(max_length=200)
+    emoji = models.CharField(max_length=200)
+
+class Entry(models.Model):
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.mood.emoji
+
+ 
